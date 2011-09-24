0ef7604638a72e8cc7b2d8c7ce5ea86e1d4e3d08
Michael Ummels <michael@ummels.de>
2011-09-24 18:48:27 +0200

    Better indentation in Python script

--- scripts/finddeps.py
+++ scripts/finddeps.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python
 
-import sys, re, os.path
+import sys
+import re
+import os.path
 
 res = set()
 stack = [os.path.normpath(x) for x in sys.argv[1:]]
 expr = re.compile("input\s+([\w.]+)")
 
 while stack:
-	current = stack.pop() # Pop from stack
-	try:
-		basedir = os.path.dirname(current)
-		with open(current, 'r') as f:
-			matches = expr.findall(f.read())
-		for r in matches:
-			if not "." in r: r = r + ".mf"
-			r = os.path.normpath(os.path.join(basedir, r))
-			if os.path.exists(r) and r not in res:
-				stack.append(r) # Push on stack
-				res.add(r)
-	except IOError:
-		pass
+    current = stack.pop() # Pop from stack
+    try:
+        basedir = os.path.dirname(current)
+        with open(current, 'r') as f:
+            matches = expr.findall(f.read())
+        for r in matches:
+            if not "." in r: r = r + ".mf"
+            r = os.path.normpath(os.path.join(basedir, r))
+            if os.path.exists(r) and r not in res:
+                stack.append(r) # Push on stack
+                res.add(r)
+    except IOError:
+        pass
 
 if res:
-	print reduce(lambda x, y: x + " " + y, res)
+    print reduce(lambda x, y: x + " " + y, res)
